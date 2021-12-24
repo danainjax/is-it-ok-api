eb222e77a16ea43a27f404e33911cac65d7cb9b8
danainjax <danainjax@gmail.com>
2021-12-24 12:10:11 -0500

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,21 @@ class UsersController < ApplicationController
         render json: users
     end
 
+    def create
+        user = User.new(user_params)
+        if user.save
+            render user
+        else
+            render json: { errors: errors.full_messages.to_sentence}, status: :unprocessable_entity
+        end
+    end
+
     def show
         @user 
         render json: @user
     end
+
+    
 
     private
 
