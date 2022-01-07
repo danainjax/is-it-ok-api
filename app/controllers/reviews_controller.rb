9540fc0479272d1ee920f3deb3476e41101dcafd
danainjax <danainjax@gmail.com>
2022-01-06 20:20:29 -0500

    added create and params to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+
+    def create
+        logged_in_user.reviews.create(review_params)
+        render json: review
+    end
+
+
+
+private
+    def review_params
+        params.permit(:movie_id, :rating, :sex, :violence, :language, :comment)
+    end
 end
