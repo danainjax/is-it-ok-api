11ae0af3ce86cadbaab19a9f351a89d9cce73196
danainjax <danainjax@gmail.com>
2022-01-06 19:41:09 -0500

    updated restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :movies, only: [:index, :show]
-  resources :users
+  resources :users, only:[:create]
+  resources :movies, only: [:index, :show] do
+    resources :reviews, only:[:create]
+  end
   resources :sessions, only: [:create]
   get "/me", to: 'sessions#autologin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
